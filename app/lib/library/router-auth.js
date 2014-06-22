d124a2ba1a69e3c177283bf7a17a222e972d03cc
Venkat Dinavahi <vendiddy@gmail.com>
2014-06-21 20:25:43 -0400

    added project loading

--- app/lib/library/router-auth.js
+++ app/lib/library/router-auth.js
@@ -1,11 +1,19 @@
 Ember.Route.reopen({
   requiresAuth: false,
-  beforeModel: function() {
+
+  beforeModel: function(transition) {
     var route = this;
+
     if (this.get('requiresAuth')) {
       return this.get('auth').checkStatus().then(function(user) {
         return user;
       }, function(error) {
+        route.unauthenticated('unauthenticated');
+      }).then(function() {
+        return route.get('documentSource').load(transition.params.document.document_id);
+      }).then(function(doc) {
+        return doc;
+      }, function(reason) {
         route.unauthenticated('unauthenticated');
       });
     }
