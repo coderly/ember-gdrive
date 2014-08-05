5f1cac5f99624c6fac6920f0031a428e3d5806d9
Venkat Dinavahi <vendiddy@gmail.com>
2014-08-04 21:31:02 -0400

    fixed async record situation

--- dist/commonjs/library/serializer.js
+++ dist/commonjs/library/serializer.js
@@ -15,12 +15,23 @@ var serializeRecordPolymorphicId = function(record) {
 var Serializer = DS.JSONSerializer.extend({
 
   serializeHasMany: function(record, json, relationship) {
-    var key = relationship.key,
-        relationshipType = DS.RelationshipChange.determineRelationshipType(record.constructor, relationship);
+    var key = relationship.key;
+    var serializeId = relationship.options.polymorphic ? serializeRecordPolymorphicId : serializeRecordId;
+    var rel = record.get(key);
+    if(relationship.options.async){
+      rel = rel.get('content');
+    }
+    if (rel){
+      json[key] = rel.map(serializeId);
+    }
+  },
 
-    if (relationshipType === 'manyToNone' || relationshipType === 'manyToMany') {
-      var serializeId = relationship.options.polymorphic ? serializeRecordPolymorphicId : serializeRecordId;
-      json[key] = Ember.get(record, key).map(serializeId);
+  serializeBelongsTo: function(record, json, relationship) {
+    if (relationship.options && relationship.options.async){
+      var key = relationship.key;
+      json[key] = record.get(relationship.key).get('content.id');
+    } else {
+      this._super(record, json, relationship);
     }
   }
 
