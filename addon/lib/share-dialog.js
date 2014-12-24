98612a5308d171573edeb31c3f1544f93289ddb0
Nikola Begedin <begedinnikola@gmail.com>
2014-12-24 17:36:47 +0100

    Updated config property paths

--- addon/lib/share-dialog.js
+++ addon/lib/share-dialog.js
@@ -1,9 +1,10 @@
 import Ember from 'ember';
+import Config from '../config/environment';
 
 export default Ember.Object.extend({
   documentId: Ember.required(),
   show: function() {
-    var shareClient = new gapi.drive.share.ShareClient(ENV.GOOGLE_DRIVE_SDK_APP_ID);
+    var shareClient = new gapi.drive.share.ShareClient(Config['ember-gdrive'].GOOGLE_DRIVE_SDK_APP_ID);
 
     shareClient.setItemIds([ this.get('documentId') ]);
     shareClient.showSettingsDialog();
