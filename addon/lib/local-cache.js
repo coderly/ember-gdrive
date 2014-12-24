7a7cc08e8ce5ea625ce442fc34cf850cb7f5fb2e
Nikola Begedin <begedinnikola@gmail.com>
2014-12-24 16:25:21 +0100

    Integrated ember-simple-auth into addon

---
+++ addon/lib/local-cache.js
@@ -0,0 +1,13 @@
+/* globals localStorage*/
+
+var LocalCache = function(namespace) {
+  this.namespace = namespace;
+};
+LocalCache.prototype.get = function(id) {
+  return localStorage.getItem(this.namespace + ':' + id);
+};
+LocalCache.prototype.set = function(id, value) {
+  localStorage.setItem(this.namespace + ':' + id, value);
+};
+
+export default LocalCache;
