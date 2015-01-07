9673e7bef136c39d9262e6f1129af03f6cce793f
Nikola Begedin <begedinnikola@gmail.com>
2015-01-07 18:50:29 +0100

    Added contentFor hook with gapi script injection to index.js

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 module.exports = {
   name: 'ember-gdrive',
+  
+  contentFor: function(type) {
+    if (type === 'head') {
+      return '<script type="text/javascript" src="https://apis.google.com/js/api.js"></script>';
+    }
+  },  
   included: function (app) {
     app.import('vendor/share-modal.css');
   }
