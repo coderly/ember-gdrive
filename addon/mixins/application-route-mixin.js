2bcd61c9584e878e2781d23c0224873a682bd69b
begedin <begedinnikola@gmail.com>
2015-01-12 20:12:15 +0100

    Moved login logic to addon
    Resolves #55
    
    * `ApplicationRouteMixin` now holds logic to open document from state
    * `LoginControllerMixin` should now be used from app's login route

--- addon/mixins/application-route-mixin.js
+++ addon/mixins/application-route-mixin.js
@@ -1,3 +1,45 @@
+import Ember from 'ember';
 import ApplicationRouteMixin from 'simple-auth/mixins/application-route-mixin';
 
-export default ApplicationRouteMixin;
+export default Ember.Mixin.create(ApplicationRouteMixin, {
+  beforeModel: function (transition) {
+
+    this._super(transition);
+
+    var route = this;
+    var documentSource = route.get('documentSource');
+    var state = documentSource.get('state'); // Forces creation of the state, which clears the query params
+    var isStatePresent = state.get('isOpen') || state.get('isCreate');
+
+    if (!isStatePresent) {
+      return;
+    }
+
+    this.get('session').authenticate('authenticator:gdrive').then(function () {
+
+      if (state.get('isOpen')) {
+        return documentSource.openFromState();
+      } else {
+        return documentSource.createFromState();
+      }
+    }).then(function (doc) {
+      if (doc) {
+        route.transitionTo('document', doc);
+      }
+    });
+  },
+  
+  actions: {
+    login: function () {
+      this.session.authenticate('authenticator:gdrive');
+    },
+    logout: function () {
+      var route = this,
+        session = this.get('session');
+
+      session.invalidate().then(function () {
+        route.transitionTo('login');
+      });
+    }
+  }
+});
