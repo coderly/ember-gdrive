c3a01c43addacfb0f16de8697176e4c475ce8f8b
begedin <begedinnikola@gmail.com>
2015-01-15 18:56:26 +0100

    Added blueprint generator files.
    These files are usually added automatically when running `ember generate blueprint 'blueprint-name'`. Now that `ember-cli` was updated, we have access to this command.

--- blueprints/ember-gdrive/index.js
+++ blueprints/ember-gdrive/index.js
@@ -1,11 +1,12 @@
 module.exports = {
-  normalizeEntityName: function() {},
- 
-  afterInstall: function() {
+  description: '',
+
+  afterInstall: function (options) {
     var that = this;
  
     return this.addBowerPackageToProject('ember-simple-auth').then(function () {
       return this.addAddonToProject('ember-cli-simple-auth');
     });
+
   }
 };
