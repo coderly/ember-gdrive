bb9e59a6846fd594ad546d506b9ae72b8bf15b6d
begedin <begedinnikola@gmail.com>
2015-01-15 19:34:23 +0100

    Fixed default blueprint behavior
    * `normalizeEntityName` is required as an empty function, as a workaround for the default add-on not having an entity name. I'm hoping this will not be required in a future ember-cli version.
    * While the `ember destroy` command runs the uninstall method in a blueprint and there's an `afterUninstall` hook for blueprints, there doesn't seem to be a method that could be used to remove a package from a project, so I didn't add any of that yet. A task will be created to add this once it becomes available. Currently, the only way to uninstall add-on and it's dependencies seems to be by manual deletion (via npm/bower, or actual hard-drive deletion).

--- blueprints/ember-gdrive/index.js
+++ blueprints/ember-gdrive/index.js
@@ -1,12 +1,24 @@
 module.exports = {
-  description: '',
+  normalizeEntityName: function () {
+    // this prevents an error when the entityName is
+    // not specified (since that doesn't actually matter
+    // to us
+  },
 
   afterInstall: function (options) {
-    var that = this;
- 
-    return this.addBowerPackageToProject('ember-simple-auth').then(function () {
-      return this.addAddonToProject('ember-cli-simple-auth');
-    });
+    var addonContext = this;
 
+    return this.addBowerPackageToProject('ember-simple-auth')
+      .then(function () {
+        return addonContext.addPackageToProject('ember-cli-simple-auth');
+      });
+  },
+  
+  afterUninstall: function (options) {
+    // Ideally, we would remove the extra packages we added during 'afterInstall'
+    // from within this hook. However, no such option seems to be available at the  
+    // moment
+    
+    // In fact, I'm not even finding an ember uninstall:<type> command at the moment
   }
 };
