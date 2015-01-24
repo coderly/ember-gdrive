180a5967394d5b054b7714f0a21609ef3cd7e012
Nikola Begedin <begedinnikola@gmail.com>
2015-01-24 15:28:04 +0100

    Add support for custom-fixture-adapter
    Moved to ember-gdrive from Storypad

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,3 +1,6 @@
 import GoogleDriveAdapter from 'ember-gdrive/adapters/google-drive';
+import CustomFixtureAdapter from 'ember-gdrive/adapters/custom-fixture';
 
-export default GoogleDriveAdapter;
+var Adapter = window.testDocument ? CustomFixtureAdapter.extend() : GoogleDriveAdapter.extend();	
+
+export default Adapter;
