79c5e492f034110a265f1b57bdd82e1a96213f2f
Nikola Begedin <begedinnikola@gmail.com>
2015-01-24 15:38:54 +0100

    Removed custom-fixture adapter.
    Appears to be unused and removed from storypad in 4f4175fdf829060eb333a05c61657e0cf408dea2

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,6 +1,3 @@
 import GoogleDriveAdapter from 'ember-gdrive/adapters/google-drive';
-import CustomFixtureAdapter from 'ember-gdrive/adapters/custom-fixture';
 
-var Adapter = window.testDocument ? CustomFixtureAdapter.extend() : GoogleDriveAdapter.extend();	
-
-export default Adapter;
+export default GoogleDriveAdapter;
