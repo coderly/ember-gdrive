5da114150be1752da1a356b35fee2e8d119106b4
begedin <begedinnikola@gmail.com>
2015-01-25 19:22:06 +0100

    Moved share-modal.css to addon/styles/share-modal.css
    Previously, the `share-modal.css` was laced in the `vendor` folder and being imported manually via the `included` hook inside `index.js`.
    
    The more correct way to include add-on specific styles is by placing them inside the `addon/styles` folder. All .css files included there are automatically merged into `vendor.css`.

--- index.js
+++ index.js
@@ -5,8 +5,5 @@ module.exports = {
     if (type === 'head') {
       return '<script type="text/javascript" src="https://apis.google.com/js/api.js"></script>';
     }
-  },  
-  included: function (app) {
-    app.import('vendor/share-modal.css');
   }
 };
