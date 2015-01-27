ba32a9b58d3e54f5cd946e034f4ed19cc255c0a8
begedin <begedinnikola@gmail.com>
2015-01-27 20:25:23 +0100

    Added gapi properties placeholder to config via blueprint

--- blueprints/ember-gdrive/index.js
+++ blueprints/ember-gdrive/index.js
@@ -7,6 +7,14 @@ module.exports = {
 
   afterInstall: function (options) {
     var addonContext = this;
+    
+    this.insertIntoFile('config/environment.js', 
+        '\n      \'ember-gdrive\': {' +
+        '\n        GOOGLE_API_KEY: \'<insert here>\',' +
+        '\n        GOOGLE_MIME_TYPE: \'application/\',' +
+        '\n        GOOGLE_DRIVE_SDK_APP_ID: \'<insert here>\',' +
+        '\n        GOOGLE_CLIENT_ID: \'<insert here>\'' + 
+        '\n      },\n' , { after: 'APP: {' });
 
     return this.addBowerPackageToProject('ember-simple-auth', '~0.7.2')
       .then(function () {
