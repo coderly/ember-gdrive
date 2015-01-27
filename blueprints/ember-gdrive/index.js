8e6470b49a00e7468cacae33fc34250a78f2e3cd
begedin <begedinnikola@gmail.com>
2015-01-27 20:38:16 +0100

    Improved formatting and chaining

--- blueprints/ember-gdrive/index.js
+++ blueprints/ember-gdrive/index.js
@@ -7,17 +7,19 @@ module.exports = {
 
   afterInstall: function (options) {
     var addonContext = this;
-    
-    this.insertIntoFile('config/environment.js', 
-        '\n      \'ember-gdrive\': {' +
-        '\n        GOOGLE_API_KEY: \'<insert here>\',' +
-        '\n        GOOGLE_MIME_TYPE: \'application/\',' +
-        '\n        GOOGLE_DRIVE_SDK_APP_ID: \'<insert here>\',' +
-        '\n        GOOGLE_CLIENT_ID: \'<insert here>\'' + 
-        '\n      },\n' , { after: 'APP: {' });
 
-    return this.addBowerPackageToProject('ember-simple-auth', '~0.7.2')
+    this.insertIntoFile('config/environment.js',
+      '\n      \'ember-gdrive\': {' +
+      '\n        GOOGLE_API_KEY: \'<insert here>\',' +
+      '\n        GOOGLE_MIME_TYPE: \'application/<insert here>\',' +
+      '\n        GOOGLE_DRIVE_SDK_APP_ID: \'<insert here>\',' +
+      '\n        GOOGLE_CLIENT_ID: \'<insert here>\'' +
+      '\n      },\n', {
+        after: 'APP: {'
+      })
       .then(function () {
+        return this.addBowerPackageToProject('ember-simple-auth', '~0.7.2');
+      }).then(function () {
         return addonContext.addAddonToProject('ember-cli-simple-auth', '~0.7.2');
       });
   }
