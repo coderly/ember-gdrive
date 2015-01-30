e1e5c6f46ff7170ce7295d974f042ef5574c2b29
Nikola Begedin <begedinnikola@gmail.com>
2015-01-30 22:14:44 +0100

    Fixed dependencies not being installed properly upon add-on installation

--- blueprints/ember-gdrive/index.js
+++ blueprints/ember-gdrive/index.js
@@ -8,19 +8,19 @@ module.exports = {
   afterInstall: function (options) {
     var addonContext = this;
 
-    this.insertIntoFile('config/environment.js',
-      '\n      \'ember-gdrive\': {' +
-      '\n        GOOGLE_API_KEY: \'<insert here>\',' +
-      '\n        GOOGLE_MIME_TYPE: \'application/<insert here>\',' +
-      '\n        GOOGLE_DRIVE_SDK_APP_ID: \'<insert here>\',' +
-      '\n        GOOGLE_CLIENT_ID: \'<insert here>\'' +
-      '\n      },\n', {
-        after: 'APP: {'
-      })
-      .then(function () {
-        return this.addBowerPackageToProject('ember-simple-auth', '~0.7.2');
-      }).then(function () {
-        return addonContext.addAddonToProject('ember-cli-simple-auth', '~0.7.2');
-      });
+    return addonContext.addBowerPackageToProject('ember-simple-auth', '~0.7.2')
+    .then(function () {
+      return addonContext.addAddonToProject('ember-cli-simple-auth', '~0.7.2');
+    }).then(function () {
+      return addonContext.insertIntoFile('config/environment.js',
+        '\n      \'ember-gdrive\': {' +
+        '\n        GOOGLE_API_KEY: \'<insert here>\',' +
+        '\n        GOOGLE_MIME_TYPE: \'application/<insert here>\',' +
+        '\n        GOOGLE_DRIVE_SDK_APP_ID: \'<insert here>\',' +
+        '\n        GOOGLE_CLIENT_ID: \'<insert here>\'' +
+        '\n      },\n', {
+          after: 'APP: {'
+        });
+    });
   }
 };
