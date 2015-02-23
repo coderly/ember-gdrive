dd463d341d9d5d1eaa657df8c83ab18e239cac1f
begedin <begedinnikola@gmail.com>
2015-02-23 12:36:29 +0100

    Removed automatic filling-out of content security policy
    It doesn't actually work.

--- index.js
+++ index.js
@@ -7,38 +7,6 @@ module.exports = {
     }
   },
   
-  config: function (environment, baseConfig) {
-    
-    var config = {};
-    
-    config.contentSecurityPolicyHeader = 'Content-Security-Policy';
-    config.contentSecurityPolicy = baseConfig.contentSecurityPolicy || {};
-    var requiredCSP = {
-      'default-src': 'accounts.google.com content.googleapis.com drive.google.com',
-      'script-src': '\'unsafe-eval\' \'unsafe-inline\' apis.google.com drive.google.com',
-      'connect-src': '\'unsafe-eval\' apis.google.com drive.google.com',
-      'img-src': 'data: ssl.gstatic.com csi.gstatic.com',
-      'style-src': '\'unsafe-inline\''
-    };
-    
-    if (config.contentSecurityPolicy['default-src'] === '\'none\'') {
-      config.contentSecurityPolicy['default-src'] = '';
-    }
-    
-    var mergeValues = function (item) {
-      if (!config.contentSecurityPolicy[propertyName]) {
-        config.contentSecurityPolicy[propertyName] = item;
-      } else if (config.contentSecurityPolicy[propertyName].indexOf(item) === -1) {
-        config.contentSecurityPolicy[propertyName] += ' ' + item;
-      }
-    };
-    
-    for (var propertyName in requiredCSP) {
-      requiredCSP[propertyName].split(' ').forEach(mergeValues);
-    }
-    
-    return config;
-  },  
   included: function (app) {
     app.import('vendor/share-modal.css');
   }
